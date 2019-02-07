cc570df4f4e8191542b3c070451369247f36b4dc
Louie O'Grady <l.ogrady@gmail.com>
2019-02-07 14:15:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,7 +12,7 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-	monopoly = {:railroads => {}, :names => {}}
+	monopoly = {:railroads => {:pieces => 4, :names => {}}
 end
 
 def monopoly_with_third_tier
